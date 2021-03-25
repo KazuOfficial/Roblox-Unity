380c37b4c5885d1e79da75c1456204f5c7d7b84c
KazuOfficial <kazuofficial.contact@gmail.com>
2021-03-25 16:41:33 +0100

    Added levels: Crossroads, Yoricks Resting Place, Rocket Arena

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -11,6 +11,16 @@ public class Menu : MonoBehaviour
         SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
     }
 
+    public void Crossroads()
+    {
+        SceneManager.LoadScene("Crossroads", LoadSceneMode.Single);
+    }
+
+    public void YoricksRestingPlace()
+    {
+        SceneManager.LoadScene("Yorick's Resting Place", LoadSceneMode.Single);
+    }
+
     public void Play()
     {
         SceneManager.LoadScene("Play", LoadSceneMode.Single);
